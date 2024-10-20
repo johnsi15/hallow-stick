018d29a98c4dcdcb2d3a3d9e65ee454533262fac
John Serrano <andrey_201015@hotmail.com>
2024-10-20 18:40:13 -0500

    Add adapter netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from 'astro/config'
 
 import tailwind from '@astrojs/tailwind'
+import netlify from '@astrojs/netlify'
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,4 +11,5 @@ export default defineConfig({
     contentLayer: true,
   },
   integrations: [tailwind()],
+  adapter: netlify(),
 })
